7f73927a1e733bfdcb3256c9f8a9882897747b06
Ricardo Freires de Andrade <ricardo.freires@gmail.com>
2022-09-29 20:03:09 -0300

    Componente criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,11 +26,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// Componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
