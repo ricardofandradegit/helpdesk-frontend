3245a3cd942faae51df404c5c89b85a56dee0b56
Ricardo Freires de Andrade <ricardo.freires@gmail.com>
2022-09-30 21:23:13 -0300

    Elemento TABLE para o TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Ricardo Freires de Andrade',
+      cpf: '887.729.184-20',
+      email: 'ricardo.freires@gmail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '30/09/2022'
+    }
+  ]
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
