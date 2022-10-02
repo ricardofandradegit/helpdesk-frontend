083842a016c6537aaaa5f0b46bdf177a6b17f75f
Ricardo Freires de Andrade <ricardo.freires@gmail.com>
2022-10-02 15:45:56 -0300

    Rota para componente TECNICO-CREATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './login/login.component';
 
@@ -13,7 +14,8 @@ const routes: Routes = [
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
-      { path: 'tecnicos', component: TecnicoListComponent}
+      { path: 'tecnicos', component: TecnicoListComponent},
+      { path: 'tecnicos/create', component: TecnicoCreateComponent}
     ]
   }
 ];
